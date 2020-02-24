af0cbf1afdf20370f3af2da9e204f56ff48e5a72
shiruz <54540460+shiruz@users.noreply.github.com>
2020-02-24 14:48:41 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * A callback function for LLEMU's center button. test
+ * A callback function for LLEMU's center button. sheryl
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
